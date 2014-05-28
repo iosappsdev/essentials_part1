657d280234804b805d55bb93bdbf9571ffe17b7b
CtecTeacher <ctecteacher@H8-T-607853.local>
2014-05-27 19:22:03 -0700

    Initial Commit

---
+++ functions/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  functions
+//
+//  Created by CtecTeacher on 5/27/14.
+//  Copyright (c) 2014 ABC Adult School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ functions/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  functions
+//
+//  Created by CtecTeacher on 5/27/14.
+//  Copyright (c) 2014 ABC Adult School. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
